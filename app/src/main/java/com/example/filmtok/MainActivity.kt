f3726d4c931ceb11c4e600245f5c46482aca59c7
Dominik Gabis <dominikgabis04@gmail.com>
2026-04-16 14:18:20 +0200

    added firebase references

--- app/src/main/java/com/example/filmtok/MainActivity.kt
+++ app/src/main/java/com/example/filmtok/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.example.filmtok
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
 import com.google.firebase.FirebaseApp
+import com.google.firebase.firestore.firestore
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,5 +21,16 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+//        val db = Firebase.firestore
+//        val testData = hashMapOf("status" to "FilmTok działa!")
+//
+//        db.collection("polaczenie_testowe")
+//            .add(testData)
+//            .addOnSuccessListener {
+//                Log.d("FirebaseTest", "Sukces! Dane są w chmurze.")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.w("FirebaseTest", "Błąd połączenia", e)
+//            }
     }
 }
